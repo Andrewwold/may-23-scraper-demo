7e92e24bc3c5b8ddcb02ca1ae3027bef5e77a658
Andrew <goldsabre@gmail.com>
2020-05-23 16:58:44 -0600

    added ability to sort out links by how often they show up in the scrape

--- scraper.py
+++ scraper.py
@@ -3,58 +3,58 @@ import statistics
 from bs4 import BeautifulSoup
 
 
-baseball_websites = [
-"https://www.mlb.com/news"
-]
-
 # baseball_websites = [
-# "https://www.mlb.com/news",
-# "https://www.mlbtraderumors.com",
-# "https://www.fangraphs.com/blogs/",
-# "http://www.milb.com/index.jsp",
-# "https://www.yardbarker.com/mlb",
-# "https://www.baseballprospectus.com",
-# "https://news.google.com/?q=baseball&tbm=nws&hl=en-US&gl=US&ceid=US:en", 
-# "https://www.reddit.com/r/baseball",
-# "https://blog.justbats.com",
-# "https://nypost.com/baseball",
-# "https://razzball.com",
-# "https://www.minorleagueball.com",
-# "https://d1baseball.com",
-# "https://www.royalsreview.com",
-# "https://sabr.org",
-# "https://www.brewcrewball.com",
-# "https://mlbcomblogs.mlblogs.com",
-# "https://federalbaseball.com",
-# "https://bluejaysnation.com",
-# "https://purplerow.com",
-# "https://beyondtheboxscore.com",
-# "https://southsidesox.com",
-# "http://dodgersdigest.com",
-# "https://www.baseballthinkfactory.org/newsstand",
-# "https://drivelinebaseball.com/blog",
-# "https://whatproswear.com",
-# "https://gaslampball.com",
-# "https://justbatreviews.com/blog",
-# "https://baseballfactory.com/news",
-# "http://joeposnanski.com",
-# "https://redlegnation.com",
-# "https://puckettspond.com",
-# "http://bronxbaseballdaily.com",
-# "http://jaysfromthecouch.com",
-# "http://collegebaseballdaily.com",
-# "http://2018youthbats.com/blog",
-# "http://insiderbaseball.com/blog",
-# "http://thebaseballdiamond.com",
-# "http://thecubreporter.com",
-# "http://americanassociationbaseball.com",
-# "http://offthebenchbaseball.com",
-# "http://birdswatcher.com",
-# "http://baseballreflections.com",
-# "http://rayscoloredglasses.com",
-# "http://mister-baseball.com",
-# "http://216stitches.org"
+# "https://www.mlb.com/news"
 # ]
+
+baseball_websites = [
+"https://www.mlb.com/news",
+"https://www.mlbtraderumors.com",
+"https://www.fangraphs.com/blogs/",
+"http://www.milb.com/index.jsp",
+"https://www.yardbarker.com/mlb",
+"https://www.baseballprospectus.com",
+"https://news.google.com/?q=baseball&tbm=nws&hl=en-US&gl=US&ceid=US:en", 
+"https://www.reddit.com/r/baseball",
+"https://blog.justbats.com",
+"https://nypost.com/baseball",
+"https://razzball.com",
+"https://www.minorleagueball.com",
+"https://d1baseball.com",
+"https://www.royalsreview.com",
+"https://sabr.org",
+"https://www.brewcrewball.com",
+"https://mlbcomblogs.mlblogs.com",
+"https://federalbaseball.com",
+"https://bluejaysnation.com",
+"https://purplerow.com",
+"https://beyondtheboxscore.com",
+"https://southsidesox.com",
+"http://dodgersdigest.com",
+"https://www.baseballthinkfactory.org/newsstand",
+"https://drivelinebaseball.com/blog",
+"https://whatproswear.com",
+"https://gaslampball.com",
+"https://justbatreviews.com/blog",
+"https://baseballfactory.com/news",
+"http://joeposnanski.com",
+"https://redlegnation.com",
+"https://puckettspond.com",
+"http://bronxbaseballdaily.com",
+"http://jaysfromthecouch.com",
+"http://collegebaseballdaily.com",
+"http://2018youthbats.com/blog",
+"http://insiderbaseball.com/blog",
+"http://thebaseballdiamond.com",
+"http://thecubreporter.com",
+"http://americanassociationbaseball.com",
+"http://offthebenchbaseball.com",
+"http://birdswatcher.com",
+"http://baseballreflections.com",
+"http://rayscoloredglasses.com",
+"http://mister-baseball.com",
+"http://216stitches.org"
+]
 
 def ranking(websites):
 	link_dic = {}
@@ -69,13 +69,26 @@ def ranking(websites):
 				link_dic[key] = 1
 	
 	numbers = [link_dic[key] for key in link_dic]
-	print(numbers)
+	med = 10
+	low = []
+	high = []
+	for key in link_dic:
+		if link_dic[key] <= med:
+			low.insert(len(low), key)
+		else:
+			high.insert(len(high), key)
+
+	return low, high
 
 
 
-ranking(baseball_websites)
+low, high = ranking(baseball_websites)
 
+def looper(list):
+	for link in list:
+		print(link)
 
+looper(high)
 
 
 
